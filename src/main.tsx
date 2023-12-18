d19453cdadf16d35930508952904d745daa4b38c
Sanarielsen <sanarielsen.dev@gmail.com>
2023-12-18 02:14:14 -0300

    AAA

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// const routerBaseName = process.env.PUBLIC_URL;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 )
